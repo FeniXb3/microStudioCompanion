766edefbba865dd5b4b9bb6518d7d584537eeab0
FeniXb3 <fenix.b3@gmail.com>
2021-01-07 01:34:11 +0100

    Not set background color when not needed

--- microStudioCompanion/Logger.cs
+++ microStudioCompanion/Logger.cs
@@ -13,7 +13,6 @@ namespace microStudioCompanion
         private static ConsoleColor infoColor = ConsoleColor.Gray;
         private static ConsoleColor errorColor = ConsoleColor.Red;
         private static ConsoleColor queryColor = ConsoleColor.Blue;
-        private static ConsoleColor defaultBackgroundColor = ConsoleColor.Black;
 
         private static string incoming = "->";
         private static string outgoing = "<-";
@@ -21,38 +20,45 @@ namespace microStudioCompanion
 
         public static void LogOutgoingInfo(string text, bool withNewLine = true)
         {
-            Log(text, info, outgoing, infoColor, defaultBackgroundColor, withNewLine);
+            Log(text, info, outgoing, infoColor, null, withNewLine);
         }
 
         public static void LogIncomingInfo(string text, bool withNewLine = true)
         {
-            Log(text, info, incoming, infoColor, defaultBackgroundColor, withNewLine);
+            Log(text, info, incoming, infoColor, null, withNewLine);
         }
 
         public static void LogLocalInfo(string text, ConsoleColor? textColor = null, ConsoleColor? backgroundColor = null, bool withNewLine = true)
         {
-            Log(text, info, local, textColor ?? infoColor, backgroundColor ?? defaultBackgroundColor, withNewLine);
+            Log(text, info, local, textColor ?? infoColor, backgroundColor, withNewLine);
         }
 
         internal static void LogLocalError(string text, bool withNewLine = true)
         {
-            Log(text, error, local, errorColor, defaultBackgroundColor, withNewLine);
+            Log(text, error, local, errorColor, null, withNewLine);
         }
 
         internal static void LogIncomingError(string text, bool withNewLine = true)
         {
-            Log(text, error, incoming, errorColor, defaultBackgroundColor, withNewLine);
+            Log(text, error, incoming, errorColor, null, withNewLine);
         }
 
         internal static void LogLocalQuery(string text, bool withNewLine = false)
         {
-            Log(text, query, local, queryColor, defaultBackgroundColor, withNewLine);
+            Log(text, query, local, queryColor, null, withNewLine);
         }
 
-        private static void Log(string text, string type, string source, ConsoleColor textColor, ConsoleColor backgroundColor, bool withNewLine = true)
+        private static void Log(string text, string type, string source, ConsoleColor? textColor, ConsoleColor? backgroundColor, bool withNewLine = true)
         {
-            Console.ForegroundColor = textColor;
-            Console.BackgroundColor = backgroundColor;
+            if (textColor.HasValue)
+            {
+                Console.ForegroundColor = textColor.Value;
+            }
+            if (backgroundColor.HasValue)
+            {
+                Console.BackgroundColor = backgroundColor.Value;
+            }
+
             Console.Write($" {source} {type} {text}");
             Console.ResetColor();
             if (withNewLine)
