de2488ac08d33335edd4f117321ffc29bf643e95
Konrad Gadzina <konrad@enklawa.blog>
2021-01-11 23:57:17 +0100

    Add listen_to_project request

---
+++ microStudioCompanion/ListenToProjectRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace microStudioCompanion
+{
+    class ListenToProjectRequest : RequestBase
+    {
+        public string user { get; set; }
+        public string project { get; set; }
+
+        public ListenToProjectRequest()
+        {
+            name = "listen_to_project";
+        }
+        public override string Serialize()
+        {
+            return System.Text.Json.JsonSerializer.Serialize(this);
+        }
+
+        public override string Message => $"Listen to project {project}";
+    }
+}
